57be411877c42abe436dc2dc5b9d23020acca01e
phanatcha <phanatcha19@gmail.com>
2026-06-09 10:30:32 +0700

    Edit whole sphere to dorm sphere

--- las-vegas-sphere/light_sphere.fs
+++ las-vegas-sphere/light_sphere.fs
@@ -1,7 +1,15 @@
 #version 410 core
 out vec4 FragColor;
 
+in vec3 localPos;
+
 void main()
 {
+    // The sphere's radius is 1.0. 
+    // The equator is at Y = 0.0.
+    // The bottom is at Y = -1.0.
+    if (localPos.y < -0.3) {
+        discard;
+    }
     FragColor = vec4(1.0);
 }
